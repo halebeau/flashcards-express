219d121753fbf46efa726fb2556d7ce63aee780f
Beau <halebeau3@gmail.com>
2022-10-27 14:08:17 -0700

    Added: card.pug route

--- app.js
+++ app.js
@@ -7,9 +7,12 @@ app.set('view engine', 'pug')
 app.get('/', (req, res) => {
   res.render('index')
 })
-app.get('/hello', (req, res) => {
-  res.send('<h1>I also love Vera!</h1>')
+
+app.get('/card', (req, res) => {
+  res.render('card', { prompt: "Who is buried in Grant's tomb?" })
 })
+
+// /sandbox
 
 app.listen(3000, () => {
   console.log('The app is running on localhost:3000!')
