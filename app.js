fd0fd3b3519e7123b094680c0b03a80051250ce2
Beau <halebeau3@gmail.com>
2022-11-02 11:16:33 -0700

    Added: cookie parser

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const Parser = require('cookie-parser')
+const cookieParser = require('cookie-parser')
 
 const app = express()
 
 app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser())
 
 app.set('view engine', 'pug')
 
@@ -16,10 +19,11 @@ app.get('/card', (req, res) => {
 })
 
 app.get('/hello', (req, res) => {
-  res.render('hello')
+  res.render('hello', {name: req.cookies.username})
 })
 
 app.post('/hello', (req, res) => {
+  res.cookie('username', req.body.username)
   res.render('hello', { name: req.body.username })
 })
 
