2a983e7719294349b60c59b013cb318d5c0b08d1
Beau <halebeau3@gmail.com>
2022-11-02 11:36:02 -0700

    Added: redirect route to hello

--- app.js
+++ app.js
@@ -1,37 +1,42 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const Parser = require('cookie-parser')
-const cookieParser = require('cookie-parser')
+const express = require("express")
+const bodyParser = require("body-parser")
+const Parser = require("cookie-parser")
+const cookieParser = require("cookie-parser")
 
 const app = express()
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cookieParser())
 
-app.set('view engine', 'pug')
+app.set("view engine", "pug")
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   const name = req.cookies.username
   if (name) {
-    res.render('index', { name: name })
+    res.render("index", { name: name })
   } else {
-    res.redirect('/hello')
+    res.redirect("/hello")
   }
 })
 
-app.get('/card', (req, res) => {
-  res.render('card', { prompt: "Who is buried in Grant's tomb?" })
+app.get("/card", (req, res) => {
+  res.render("card", { prompt: "Who is buried in Grant's tomb?" })
 })
 
-app.get('/hello', (req, res) => {
-  res.render('hello')
+app.get("/hello", (req, res) => {
+  const name = req.cookies.username
+  if (name) {
+    res.redirect("/")
+  } else {
+    res.render("hello")
+  }
 })
 
-app.post('/hello', (req, res) => {
-  res.cookie('username', req.body.username)
-  res.redirect('/')
+app.post("/hello", (req, res) => {
+  res.cookie("username", req.body.username)
+  res.redirect("/")
 })
 
 app.listen(3000, () => {
-  console.log('App is running on localhost:3000!')
+  console.log("App is running on localhost:3000!")
 })
