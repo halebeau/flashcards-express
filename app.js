31cd0107078db736d5d78cb1b80abdfe8b5c0843
Beau <halebeau3@gmail.com>
2022-11-14 17:03:01 -0800

    Added: card and index routes

--- app.js
+++ app.js
@@ -9,37 +9,11 @@ app.use(cookieParser())
 
 app.set("view engine", "pug")
 
-app.get("/", (req, res) => {
-  const name = req.cookies.username
-  if (name) {
-    res.render("index", { name })
-  } else {
-    res.redirect("/hello")
-  }
-})
+const mainRoutes = require("./routes")
+const cardRoutes = require("./routes/cards")
 
-app.get("/cards", (req, res) => {
-  res.render("card", { prompt: "Who is buried in Grant's tomb?" })
-})
-
-app.get("/hello", (req, res) => {
-  const name = req.cookies.username
-  if (name) {
-    res.redirect("/")
-  } else {
-    res.render("hello")
-  }
-})
-
-app.post("/hello", (req, res) => {
-  res.cookie("username", req.body.username)
-  res.redirect("/")
-})
-
-app.post("/goodbye", (req, res) => {
-  res.clearCookie("username")
-  res.redirect("/hello")
-})
+app.use(mainRoutes)
+app.use("/cards", cardRoutes)
 
 app.use((req, res, next) => {
   const err = new Error("Not Found")
@@ -54,5 +28,5 @@ app.use((err, req, res, next) => {
 })
 
 app.listen(3000, () => {
-  console.log("App running on localhost:3000!")
+  console.log("The application is running on localhost:3000!")
 })
